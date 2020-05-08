b9d4f530a81bfae824f83e75651acf743c52ca50
ZhenyaBugera <bugera.zhenya@gmail.com>
2020-05-08 10:30:30 +0300

    Create search based on suffix tree and compare with custom js search

---
+++ SuffixTreeSearch/main.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+	let input = document.querySelector('#input');
+	let list = document.querySelectorAll('.ul li');
+	let t = new GeneralizedSuffixTree();
+                    
+	for (let i = 0; i < list.length; i++) {
+		t.put(list[i].innerText, i)
+	}
+	input.oninput = function() {
+		let value = this.value.trim();
+		list = document.querySelectorAll('.ul li');
+
+		let t0 = performance.now();
+		if (value) {
+			var r = t.search(value,-1);
+			list.forEach((elem, i) => {
+				(r && r[i]) ?
+					elem.classList.remove('hide') :
+					elem.classList.add('hide')
+			});
+		} else {
+			list.forEach(elem => {
+				elem.classList.remove('hide');
+			});
+		}
+		let t1 = performance.now();
+		console.log("tree ", t1 - t0);
+
+		t0 = performance.now();
+		value
+			? list.forEach(elem => {
+					elem.innerText.search(value) == -1
+						? elem.classList.add('hide')
+						: elem.classList.remove('hide');
+			  })
+			: list.forEach(elem => {
+					elem.classList.remove('hide');
+			  });
+		t1 = performance.now();
+		console.log(t1 - t0);
+	};
+};
